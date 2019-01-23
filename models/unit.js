8a60f3f85d8c1406f8d093e53e8ce6a1936b5e02
watsbeat <c.watsonbeaton@gmail.com>
2019-01-23 14:42:40 +1000

    created routes for units

--- models/unit.js
+++ models/unit.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const UnitSchema = new Schema({
     name: String,
-    guidehut_id: String,
+    guidehut_id: String, // How does this work? Do we have a GuideHut table?
     user_id: String,
     ageLevel: AgeLevelSchema,
 });
---
+++ routes/units.js
@@ -0,0 +1,36 @@
+const express = require ('express');
+const router = express.Router();
+const Unit = require('../models/unit');
+
+// GET /units (R)
+router.get('/unit', (req, res, next) => {
+  //this will return all the data
+  Unit.find({})
+    .then(data => res.json(data))
+    .catch(next)
+});
+
+// Only allow registered users to post and delete units
+// router.use(requireJwt)
+
+// POST /units (C)
+router.post('/units', (req, res, next) => {
+  if(req.body){
+    Unit.create(req.body)
+      .then(data => res.json(data))
+      .catch(next)
+  } else {
+    res.json({
+      error: "You need to write something here." // input field is empty
+    })
+  }
+});
+
+// DELETE /units/:id (D)
+router.delete('/programs/:id', (req, res, next) => {
+  Unit.findOneAndDelete({"_id": req.params.id})
+    .then(data => res.json(data))
+    .catch(next)
+})
+
+module.exports = router;
